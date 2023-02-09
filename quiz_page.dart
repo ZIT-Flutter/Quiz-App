9bbcc5550766fef62910d9d4ac4329ab8586e7ae
ZIT-Flutter <120543444+ZIT-Flutter@users.noreply.github.com>
2023-02-09 17:09:24 +0600

    Update quiz_page.dart

--- quiz_page.dart
+++ quiz_page.dart
@@ -1,37 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/models/quizModel.dart';
+
 class QuizPage extends StatefulWidget {
-
-  final String recievedName;
-
-
-  const QuizPage({Key? key, required this.recievedName}) : super(key: key);
-
   @override
   State<QuizPage> createState() => _QuizPageState();
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int quizIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Your Quiz'),),
-
+      appBar: AppBar(
+        title: Text('Your Quiz'),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('\nHello ${widget.recievedName}. \n\nStart Your Quiz\n\n\n', style: TextStyle(fontSize: 25),),
-            Text('Who is inventor of Programming Language? \n\n'),
-            Text('1) Person 1'),
-            Text('2) Bill gates'),
-            Text('3) Bill gates'),
-            Text('4) Bill gates'),
-
-            Text('(20+3)'),
-
-
-          ],
+        child: Container(
+          padding: EdgeInsets.all(25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(allQuiz[quizIndex].question, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+              Container(
+                height: 50,
+                width: double.infinity,
+                child: Center(
+                  child: ListView.builder(
+                    itemCount: allQuiz[quizIndex].answers.length,
+                    itemBuilder: (context, index) => Center(
+                        child: Text(allQuiz[quizIndex].answers[index].answer)),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (quizIndex < allQuiz.length - 1) {
+                        quizIndex++;
+                      }
+                    });
+                  },
+                  child: Text(quizIndex == allQuiz.length - 1
+                      ? 'Submit'
+                      : 'Next Quiz')),
+            ],
+          ),
         ),
       ),
     );
