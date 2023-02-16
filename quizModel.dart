7ae7fbed47cebae355259d66f4ba72b6f595b598
ZIT-Flutter <120543444+ZIT-Flutter@users.noreply.github.com>
2023-02-16 11:21:24 +0600

    Update quizModel.dart

--- quizModel.dart
+++ quizModel.dart
@@ -14,23 +14,94 @@ class Answer {
 
 
 List<Quiz> allQuiz = [
-  Quiz(question: 'What is your Name?', answers: [
-    Answer(answer: 'Amena', isCorrect: true),
-    Answer(answer: 'Tanvir', isCorrect: false),
-    Answer(answer: 'Mojahid', isCorrect: false),
-    Answer(answer: 'Zihan', isCorrect: false),
-    Answer(answer: 'Anna', isCorrect: false)
-  ]),
-  Quiz(question: 'What is the capital of Bangladesh?', answers: [
-    Answer(answer: 'Gazipur', isCorrect: false),
-    Answer(answer: 'Tangail', isCorrect: false),
-    Answer(answer: 'Dhaka', isCorrect: true),
-    Answer(answer: 'Comilla', isCorrect: false),
-  ]),
-  Quiz(question: 'What is the national animal of Bangladesh?', answers: [
-    Answer(answer: 'Cat', isCorrect: false),
-    Answer(answer: 'Dog', isCorrect: false),
-    Answer(answer: 'Tiger', isCorrect: true),
-    Answer(answer: 'Cow', isCorrect: false),
-  ])
+  Quiz(
+    question: "What is the capital of Bangladesh?",
+    answerList: [
+      Answer(answer: "Dhaka", isCorrect: true),
+      Answer(answer: "Chittagong", isCorrect: false),
+      Answer(answer: "Khulna", isCorrect: false),
+      Answer(answer: "Rajshahi", isCorrect: false),
+    ],
+  ),
+  Quiz(
+    question: "Which river flows through Dhaka?",
+    answerList: [
+      Answer(answer: "Jamuna", isCorrect: false),
+      Answer(answer: "Meghna", isCorrect: true),
+      Answer(answer: "Padma", isCorrect: false),
+      Answer(answer: "Brahmaputra", isCorrect: false),
+    ],
+  ),
+  Quiz(
+    question: "Who is the national poet of Bangladesh?",
+    answerList: [
+      Answer(answer: "Rabindranath Tagore", isCorrect: false),
+      Answer(answer: "Nazrul Islam", isCorrect: false),
+      Answer(answer: "Kazi Nazrul Islam", isCorrect: true),
+      Answer(answer: "Jibanananda Das", isCorrect: false),
+    ],
+  ),
+  Quiz(
+    question: "When did Bangladesh gain independence?",
+    answerList: [
+      Answer(answer: "1947", isCorrect: false),
+      Answer(answer: "1965", isCorrect: false),
+      Answer(answer: "1971", isCorrect: true),
+      Answer(answer: "1991", isCorrect: false),
+    ],
+  ),
+  Quiz(
+    question: "What is the largest country in the world by area?",
+    answerList: [
+      Answer(answer: "Russia", isCorrect: true),
+      Answer(answer: "China", isCorrect: false),
+      Answer(answer: "USA", isCorrect: false),
+      Answer(answer: "Canada", isCorrect: false),
+    ],
+  ),
+  Quiz(
+    question: "Which is the most populous country in the world?",
+    answerList: [
+      Answer(answer: "India", isCorrect: false),
+      Answer(answer: "USA", isCorrect: false),
+      Answer(answer: "China", isCorrect: true),
+      Answer(answer: "Indonesia", isCorrect: false),
+    ],
+  ),
+  Quiz(
+    question: "What is the currency of Japan?",
+    answerList: [
+      Answer(answer: "Yuan", isCorrect: false),
+      Answer(answer: "Dollar", isCorrect: false),
+      Answer(answer: "Euro", isCorrect: false),
+      Answer(answer: "Yen", isCorrect: true),
+    ],
+  ),
+  Quiz(
+    question: "Where is the tallest building in the world located?",
+    answerList: [
+      Answer(answer: "Dubai", isCorrect: true),
+      Answer(answer: "New York", isCorrect: false),
+      Answer(answer: "Hong Kong", isCorrect: false),
+      Answer(answer: "Shanghai", isCorrect: false),
+    ],
+  ),
+  Quiz(
+    question: "Which is the smallest country in the world by area?",
+    answerList: [
+      Answer(answer: "Monaco", isCorrect: true),
+      Answer(answer: "San Marino", isCorrect: false),
+      Answer(answer: "Vatican City", isCorrect: false),
+      Answer(answer: "Maldives", isCorrect: false),
+    ],
+  ),
+  Quiz(
+    question: "What is the largest ocean in the world?",
+    answerList: [
+      Answer(answer: "Atlantic Ocean", isCorrect: false),
+      Answer(answer: "Indian Ocean", isCorrect: false),
+      Answer(answer: "Arctic Ocean", isCorrect: false),
+      Answer(answer: "Pacific Ocean", isCorrect: true),
+    ],
+  )
 ];
